edcf5bdc70e73d2bd24f1ced93d48c912b319983
Ashish Kumar Panigrahy <akpanigrahy26@gmail.com>
2021-11-25 22:04:45 +0530

    Build

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,10 @@ const PORT = process.env.PORT || 4001;
 
 if (process.env.NODE_ENV === "production") {
     console.log("In Production!");
-    app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
+    app.use(express.static(path.join(__dirname, "build")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "..", "frontend", "build", "index.html"));
+        res.sendFile(path.resolve(__dirname, "build", "index.html"));
     });
 }
 
